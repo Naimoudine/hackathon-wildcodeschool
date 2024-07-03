bacb9937f4d7b3646a14277b045b3dc417bb2b6f
froustex <stephane-feroussier_student2022@wilder.school>
2024-07-03 14:42:01 +0200

    create database and insert fake data

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,8 +1,22 @@
 create table user (
   id int unsigned primary key auto_increment not null,
+  firstname varchar(100) not null,
+  lastname varchar(100) not null,
+  handicap varchar(100),
   email varchar(255) not null unique,
-  password varchar(255) not null
+  is_admin boolean
 );
+
+create table event (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  description varchar(255) not null,
+  calendar timestamp,
+  user_id int unsigned not null,
+  foreign key(user_id) references user(id),
+  is_validated boolean
+);
+
 
 create table item (
   id int unsigned primary key auto_increment not null,
